fcb712494db5f41de745229e09fb62f48527ad57
bondydaa <bondydaa@gmail.com>
2016-05-05 10:03:34 -0600

    changing url for download file

--- scripts/downloadiOSSDK.js
+++ scripts/downloadiOSSDK.js
@@ -1,20 +1,29 @@
 #!/usr/bin/env node
 
 module.exports = function (context) {
-    var IosSDKVersion = "OpenTok-iOS-2.7.1";
+    var IosSDKVersion = "OpenTok-iOS-2.8.1";
     var downloadFile = require('./downloadFile.js'),
         exec = require('./exec/exec.js'),
         Q = context.requireCordovaModule('q'),
         deferral = new Q.defer();
     console.log('Downloading OpenTok iOS SDK');
-    downloadFile('https://tokbox.com/downloads/opentok-ios-sdk-2.7.1',
+    downloadFile('https://s3.amazonaws.com/artifact.tokbox.com/rel/ios-sdk/'+IosSDKVersion+'.tar.bz2',
         './' + IosSDKVersion + '.tar.bz2', function (err) {
             if (!err) {
+                // exec('ls -la', function(err, out, code){
+                //   console.log(arguments);
+                // });
                 console.log('downloaded');
                 exec('tar -zxvf ./' + IosSDKVersion + '.tar.bz2', function (err, out, code) {
+                    // if (err) {
+                    //   console.log(err);
+                    // }
                     console.log('expanded');
                     var frameworkDir = context.opts.plugin.dir + '/src/ios/';
                     exec('mv ./' + IosSDKVersion + '/OpenTok.framework ' + frameworkDir, function (err, out, code) {
+                        if (err) {
+                          console.log(err);
+                        }
                         console.log('moved OpenTok.framework into ' + frameworkDir);
                         exec('rm -r ./' + IosSDKVersion, function (err, out, code) {
                             console.log('Removed extracted dir');
